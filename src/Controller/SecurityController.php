e30433c9dc85b00f0efe61aa7546e23f7791f7c5
Guillaume TILLARD <guillaumetillard10@gmail.com>
2025-01-07 09:59:47 +0100

    formulaire inscription

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,6 +14,12 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route('/connexion', name: 'app_login', methods: ['GET', 'POST'])]
+    /**
+     * This controller allow us to login
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
         $lastUsername = $authenticationUtils->getLastUsername();
@@ -21,8 +31,40 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/deconnexion', name: 'app_logout')]
+    /**
+     * This controller allow us to logout
+     *
+     * @return void
+     */
     public function logout()
     {
         // rien à faire ici, d'après la doc, tt va être fait lui même
+    }
+
+    #[Route('/inscription', name: 'app_registration', methods: ['GET', 'POST'])]
+    /**
+     * This controller allow us to register
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function registration(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = new User;
+        $user->setRoles(['ROLE_USER']);
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success', 'Votre compte a bien été créé');
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->render('pages/security/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
